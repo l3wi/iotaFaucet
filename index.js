1fef5ba51baec76df44009ddb476d3d6bda009cb
Lewis Freiberg <lewi@2pk.in>
2017-08-25 11:27:02 +1000

    Update to have pools

--- index.js
+++ index.js
@@ -1,35 +1,61 @@
 require('dotenv').config()
-
+var genericPool = require('generic-pool');
+const cors = require('micro-cors')()
 var IOTA = require("iota.lib.js")
-const { json } = require('micro')
+const { send, json } = require('micro')
 
-// Create IOTA instance directly with provider
-const iota = new IOTA({
-  provider: process.env.IOTA
-})
 
-const seed = process.env.SEED
+const factory = {
+  create: function(){
+   return new Promise(function(resolve, reject){
+     // Create IOTA instance directly with provider
+      const iota = new IOTA({
+        provider: process.env.IOTA
+      })
+      resolve(iota)
+    })
+  },
+  destroy: function(iota){
+    return new Promise(function(resolve, reject){
+      resolve()
+    })
+  }
+}
 
-const fundAddress = async (address) => {
+var myPool = genericPool.createPool(factory, {min: 2, max: 10})
+
+const fundAddress = async (iota, address) => {
   var transfers = [{
-    address,  value: 400
+    address: address, value: parseInt(process.env.AMOUNT), tag: `TANGLEWORKSFAUCET`
   }]
 
-  return new Promise(function(resolve, reject) {
-    iota.api.prepareTransfers(seed,
-      transfers, (e, r) => {
-      if (e) {
-        reject(e)
-      } else {
-        console.log(r)
-        resolve(r)
-      }
-    })
+  return new Promise((resolve, reject) => {
+      iota.api.prepareTransfers(process.env.SEED,
+        transfers, (e, trytes) => {
+        if (e) {
+          console.log(e)
+          reject(e)
+        } else {
+          console.log(trytes)
+          resolve(trytes)
+        }
+      })
   })
 }
 
-module.exports = async (req, res) => {
+module.exports = cors(async (req, res) => {
+  console.log("Request Recieved")    
   const js = await json(req)
-  console.log(js.address)
-  return await fundAddress(js.address)
-}
+  // Get new pool 
+  const resourcePromise = myPool.acquire()
+  // Pass back the IOTA object
+  resourcePromise.then(async (iota) => {
+    console.log("Generating an address for: ", js.address)    
+    const trytes = await fundAddress(iota, js.address)
+    send(res, 200, trytes)
+  })
+  .catch((err) => {
+     console.log(err)
+     return err
+  });
+})
