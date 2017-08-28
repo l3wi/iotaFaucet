70d8c1f33b721ba4751067f8ea4e5ab59c8dda0a
Lewis Freiberg <lewi@2pk.in>
2017-08-29 00:15:16 +1000

    Update faucet to use express

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 require("dotenv").config()
 var genericPool = require("generic-pool")
-const cors = require("micro-cors")()
 var IOTA = require("iota.lib.js")
-const { send, text } = require("micro")
+const express = require("express")
+var cors = require("cors")
+var bodyParser = require("body-parser")
+var app = express()
+
+app.use(bodyParser.json())
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  )
+  next()
+})
+// const cors = require("micro-cors")()
+
+// const { send, text } = require("micro")
 
 const factory = {
   create: function() {
@@ -50,19 +66,21 @@ const fundAddress = async (iota, address) => {
   })
 }
 
-module.exports = cors(async (req, res) => {
-  const js = JSON.parse(await text(req))
-  // Get new pool
+app.post("/", async (req, res, next) => {
   const resourcePromise = myPool.acquire()
   // Pass back the IOTA object
   resourcePromise
     .then(async iota => {
-      console.log("Generating an address for: ", js.address)
-      const trytes = await fundAddress(iota, js.address)
-      send(res, 200, trytes)
+      console.log("Generating an address for: ", req.body.address)
+      const trytes = await fundAddress(iota, req.body.address)
+      res.json(trytes)
     })
     .catch(err => {
       console.log(err)
       return err
     })
+})
+
+app.listen(3000, function() {
+  console.log("Listening on port 3000")
 })
